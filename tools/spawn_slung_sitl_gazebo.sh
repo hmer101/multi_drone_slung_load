2f2761e7d3918158ffdce7d92123276e847feb4c
Harvey <hmer101@mit.edu>
2023-03-08 17:48:39 -0500

    drone in connected group takes off (shadow spawned

--- tools/spawn_slung_sitl_gazebo.sh
+++ tools/spawn_slung_sitl_gazebo.sh
@@ -20,8 +20,9 @@ PI=3.141592654
 
 # Parameters
 PX4_SYS_AUTOSTART=4001
-PX4_GZ_MODEL_NAME=x500 #connected_group/model/x500 #swarm/model/x500
-PX4_SIM_MODEL=x500
+#PX4_GZ_MODEL=x500
+PX4_GZ_MODEL_NAME=connected_group/model/x500 #swarm/model/x500
+#PX4_SIM_MODEL=x500
 
 NUM_DRONES=2
 START_DRONE_NUM=1
@@ -37,32 +38,33 @@ function cleanup() {
 
 # Run gazebo and launch the world 
 function gz_launch_world() {
-    # cd $GZ_DIR
+    #cd $GZ_DIR
     # gnome-terminal --tab -- bash -c "gz sim ./world_multi_with_load.sdf"
-	gnome-terminal --tab -- bash -c "PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_GZ_MODEL_POSE="0,0" PX4_GZ_MODEL=$PX4_GZ_MODEL $FIRMWARE_DIR/build/px4_sitl_default/bin/px4 -i 1"
+	gnome-terminal --tab -- bash -c "PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_GZ_MODEL_POSE="0,0" PX4_GZ_MODEL=$PX4_GZ_MODEL $FIRMWARE_DIR/build/px4_sitl_default/bin/px4 -i 2" $START_DRONE_NUM
 }
 
 # Create PX4 SITL instances and connect to models in the world
 function create_sitl_instances() {
 	# Spawn all drones numbering from first selected number
-	for (( i=$(($START_DRONE_NUM + 1)); i<$(($NUM_DRONES + $START_DRONE_NUM + 1)); i++ )); do
+	for (( i=$(($START_DRONE_NUM +1)); i<$(($NUM_DRONES + $START_DRONE_NUM +1)); i++ )); do
         MODEL_NAME="${PX4_GZ_MODEL_NAME}_${i}" 
 		gnome-terminal --tab -- bash -c "PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_GZ_MODEL_NAME=$MODEL_NAME $FIRMWARE_DIR/build/px4_sitl_default/bin/px4 -i $i"
+		sleep 1
 	done
 }
 
 # RUN COMMANDS
 # Spawn drones
-cleanup
+#cleanup
 gz_launch_world
 
 sleep 5
-create_sitl_instances
+#create_sitl_instances
 sleep 2
 
 # Create multiple MAVSDK servers
 cd $SCRIPT_DIR
-gnome-terminal --tab -- bash -c "./multi_mavsdk_server.sh -n $NUM_DRONES" #$(($NUM_DRONES+1))
+gnome-terminal --tab -- bash -c "./multi_mavsdk_server.sh -n $(($NUM_DRONES))" #$(($NUM_DRONES+1))
 
 # Create ROS2 agent
 gnome-terminal --tab -- bash -c "cd ~/repos/PX4-Autopilot; micro-ros-agent udp4 --port 8888"
